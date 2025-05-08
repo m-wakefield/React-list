97be901e1233adc51bcfd072f130d666a7e8eb2f
Monique Wakefield <monique_wakefield@yahoo.com>
2025-05-08 11:53:24 -0500

    Create ContactContainer.js

---
+++ react-list/src/containers/ContactContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ContactForm from '../components/ContactForm';
+
+const ContactContainer = () => (
+  <div className="contact-container">
+    <h2>Contact Us</h2>
+    <ContactForm />
+  </div>
+);
+
+export default ContactContainer;
